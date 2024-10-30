d58e2adb7c577ca5f27ab4e03415982e33c20333
eliyya <tecno_eli@hotmail.com>
2024-10-29 20:06:53 -0600

    env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+}
+console.log(process.env)
 
-export default nextConfig;
+export default nextConfig
