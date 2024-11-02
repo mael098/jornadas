5f43fcc755a8a190dc3a37c3e8a70237043f02ac
eliyya <tecno_eli@hotmail.com>
2024-11-01 19:26:23 -0600

    active nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,24 +1,66 @@
-import Link from 'next/link'
-
+'use client'
+// CSS
 import './nav.css'
 
+import Link from 'next/link'
+import { useEffect, useState, MouseEvent, ReactElement } from 'react'
+import { useRouter } from 'next/compat/router'
+
+interface MenuOptionProps {
+    children: ReactElement
+    href: string
+}
+export function MenuOption({ children, href }: MenuOptionProps) {
+    const [active, setActive] = useState(false)
+    return (
+        <Link href={'/'} onClick={e => setActive(true)} data-active={active}>
+            {children}
+        </Link>
+    )
+}
+
 export function Nav() {
+    const [route, setRoute] = useState('/')
+    const setListener =
+        (route: string) => (e: MouseEvent<HTMLAnchorElement>) => {
+            setRoute(route)
+        }
     return (
         <nav className="tabs">
-            <Link href={'/'} className="tablink">
+            <Link
+                onClick={setListener('/')}
+                href={'/'}
+                data-active={route === '/'}
+            >
                 Página Principal
             </Link>
-            <Link href={'/expositores'} className="tablink">
+            <Link
+                onClick={setListener('/expositores')}
+                href={'/expositores'}
+                data-active={route === '/'}
+            >
                 Expositores
             </Link>
-            <Link className="tablink" href={'/talleres'}>
+            <Link
+                onClick={setListener('/talleres')}
+                href={'/talleres'}
+                data-active={route === '/'}
+            >
                 Talleres Día Jueves
             </Link>
-            <Link className="tablink" href={'/talleres-viernes'}>
+            <Link
+                onClick={setListener('/talleres-viernes')}
+                href={'/talleres-viernes'}
+                data-active={route === '/'}
+            >
                 Talleres Día Viernes
             </Link>
             {/* <!-- Nueva pestaña --> */}
-            <Link className="tablink" href={'/videojuegos'}>
+            <Link
+                onClick={setListener('/videojuegos')}
+                href={'/videojuegos'}
+                data-active={route === '/'}
+            >
                 Concurso de Videojuegos
             </Link>
             {/* <!-- Nueva pestaña --> */}
