9ba95a8dc5bb52198883bf85b9a3e647a2403537
mael098 <juanvillegasmorales@outlook.com>
2025-11-24 11:33:59 -0600

    fix: remove unnecessary gradient colors from background

--- src/app/mi-ultimo-commit/page.tsx
+++ src/app/mi-ultimo-commit/page.tsx
@@ -2,7 +2,7 @@
 
 export default function page() {
     return (
-        <div className="min-h-screen bg-linear-to-br from-slate-900 via-blue-900 to-slate-900 text-white overflow-hidden rounded-lg">
+        <div className="min-h-screen bg-linear-to-br   text-white overflow-hidden rounded-lg">
             {/* Fondo decorativo con efecto de grid */}
             <div className="fixed inset-0 opacity-10">
                 <div className="absolute inset-0 bg-linear-to-r from-cyan-500 to-blue-500 opacity-20 blur-3xl"></div>
